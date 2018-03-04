5b313a352856883df7506a009407b1341c1c0213
shha268@gmail.com <shha268@gmail.com>
2018-03-04 11:51:00 +1100

    -
    -

---
+++ 06-UsingTypeDefinitions/app.js
@@ -0,0 +1,6 @@
+//import   jquery to your project by triple/
+/// <reference path="node_modules/@types/jquery/index.d.ts" />
+$(document).ready(function () {
+    $("#hello").html("Hello World");
+    $("#hello").css('color', 'red');
+});
