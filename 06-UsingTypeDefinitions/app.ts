91c630b138cf6c95fe0ab30c8aca8066a49018ca
shha268@gmail.com <shha268@gmail.com>
2018-03-04 11:50:33 +1100

    Using Type Definition
    first take a look at "https://www.npmjs.com/~types" or "https://microsoft.github.io/TypeSearch/"
    
    installation : in your terminal type "npm install --save @types/jquery"
    
    then mport your type to your .ts file and after that you will notice that you have all your methods there!! and for e.g your $ sign in jquery works in your .ts file!!!
    that's awesome

---
+++ 06-UsingTypeDefinitions/app.ts
@@ -0,0 +1,8 @@
+//import   jquery to your project by triple/
+
+/// <reference path="node_modules/@types/jquery/index.d.ts" />
+
+$(document).ready(()=>{
+    $("#hello").html("Hello World");
+    $("#hello").css('color','red');
+});
